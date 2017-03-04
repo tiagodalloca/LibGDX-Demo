aa1b371ff63a4c578ebf4fcb71074d51ff9a3aa4
Tiago Dall'Oca <tdalloca@gmail.com>
2017-03-04 18:48:21 -0300

    As coisas estão se mechendo

--- core/src/com/planetas/demo/Demo.java
+++ core/src/com/planetas/demo/Demo.java
@@ -12,7 +12,9 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.MassData;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 
@@ -31,6 +33,9 @@ public class Demo extends ApplicationAdapter {
 	Texture img;
 	World world;
 
+	Body sunBody;
+	Body planetBody;
+
 	protected void loadAssets() {
 		am.load(SUN_FILE, Texture.class);
 		am.load(PLANET_FILE, Texture.class);
@@ -38,13 +43,13 @@ public class Demo extends ApplicationAdapter {
 
 	protected void createSprites() {
 		am.finishLoading();
-		
+
 		Texture txrSun = am.get(SUN_FILE, Texture.class);
 		Texture txrPlanet = am.get(PLANET_FILE, Texture.class);
 
 		Sprite sun = new Sprite(txrSun);
 		sun.setSize(200f, 200f);
-		
+
 		Sprite planet = new Sprite(txrPlanet);
 		planet.setSize(50f, 50f);
 
@@ -52,6 +57,8 @@ public class Demo extends ApplicationAdapter {
 
 		BodyDef sunDef = new BodyDef();
 		BodyDef planetDef = new BodyDef();
+		sunDef.type = BodyDef.BodyType.DynamicBody;
+		planetDef.type = BodyDef.BodyType.DynamicBody;
 
 		float x = (Gdx.graphics.getWidth() - sun.getWidth()) / 2.0f;
 		float y = (Gdx.graphics.getHeight() - sun.getHeight()) / 2.0f;
@@ -60,8 +67,8 @@ public class Demo extends ApplicationAdapter {
 		planetDef.position.set(new Vector2(x - 200, y));
 
 		world = new World(new Vector2(0, 0), false);
-		Body sunBody = world.createBody(sunDef);
-		Body planetBody = world.createBody(planetDef);
+		sunBody = world.createBody(sunDef);
+		planetBody = world.createBody(planetDef);
 
 		CircleShape sunCircle = new CircleShape();
 		CircleShape planetCircle = new CircleShape();
@@ -72,15 +79,36 @@ public class Demo extends ApplicationAdapter {
 		FixtureDef planetFixtureDef = new FixtureDef();
 		sunFixtureDef.shape = sunCircle;
 		planetFixtureDef.shape = planetCircle;
+		sunFixtureDef.density = 1000;
+		planetFixtureDef.density = 10;
 
 		sunBody.createFixture(sunFixtureDef);
 		planetBody.createFixture(planetFixtureDef);
-
 		sunBody.setUserData(sun);
 		planetBody.setUserData(planet);
 
 		sunCircle.dispose();
 		planetCircle.dispose();
+	}
+
+	protected void updateOrbits() {
+		double dst2 = planetBody.getPosition().dst2(sunBody.getPosition());
+		float dstX = planetBody.getPosition().x - sunBody.getPosition().x;
+		float dstY = planetBody.getPosition().y - sunBody.getPosition().y;
+
+		double bearingAngle = Math.atan2(dstX, dstY) * -1;
+
+		double f = (6.67408f * planetBody.getMass() * sunBody.getMass()) / dst2;
+		double fx = Math.sin(bearingAngle) * f;
+		double fy = Math.cos(bearingAngle) * f;
+
+		planetBody.applyForceToCenter(
+						new Vector2(new Float(fx), new Float(fy)),
+						true);
+		
+//		sunBody.applyForceToCenter(
+//						new Vector2(new Float(fx * -1), new Float(fy * -1)),
+//						true);
 	}
 
 	@Override
@@ -96,6 +124,7 @@ public class Demo extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		updateOrbits();
 		world.step(Gdx.graphics.getDeltaTime(), 6, 2);
 
 		Array<Body> bodies = new Array<Body>();
@@ -107,7 +136,7 @@ public class Demo extends ApplicationAdapter {
 				sprite.setPosition(b.getPosition().x, b.getPosition().y);
 				sprite.setRotation(MathUtils.radiansToDegrees * b.getAngle());
 			}
-			
+
 			batch.begin();
 			sprite.draw(batch);
 			batch.end();
@@ -121,7 +150,6 @@ public class Demo extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		batch.dispose();
-		img.dispose();
 		am.dispose();
 		world.dispose();
 	}
